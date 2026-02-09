0dcde6325d6ea0cf0f9850719d844089dbdd68b6
yuuuuu <bs122002@sun.ac.jp>
2026-02-09 12:12:37 +0900

    baseオプション修正

--- street_view_UI_deploy/vite.config.ts
+++ street_view_UI_deploy/vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-base: '/street-view-app/', 
+  base: '/street-view-app/', 
 })
